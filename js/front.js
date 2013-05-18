9f7ef2d641576ffb9235fba7488edc11c4dd6bd1
sony <veerpsingh@gmail.com>
2013-05-18 17:11:11 +0200

    save changes for login function

--- js/front.js
+++ js/front.js
@@ -1,6 +1,7 @@
 var global=0;
 
 
+
 function register() 
 {
 
@@ -13,4 +14,18 @@ function show_cause()
 {
   alert("Kindly show your cause that why are you late in office today");
   return false;
+}
+
+
+function login() 
+{
+
+	 /* if username is incorrect  */
+	
+	/* if password is empty      */
+
+	/* if password has more character than limit */
+
+	return false;
+
 }
