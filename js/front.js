b47dc31cde83a3fdf211bd16699e4cb03dfe4ea7
sony <veerpsingh@gmail.com>
2013-05-19 16:35:39 +0200

    save master changes to front.js file

--- js/front.js
+++ js/front.js
@@ -20,6 +20,7 @@ function show_cause()
 function login() 
 {
 	alert("Are you sure you want to login into the system");
+	alert("Kindly keep your password confidential");
 	 /* if username is incorrect  */
 	
 	/* if password is empty      */
